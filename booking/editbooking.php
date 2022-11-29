763f97f4db0535439f29c4fe78fd1bc865045c4d
MarvinWaro <114147003+MarvinWaro@users.noreply.github.com>
2022-11-30 03:15:09 +0800

    done edit

--- booking/editbooking.php
+++ booking/editbooking.php
@@ -14,7 +14,7 @@
         header('location: ../login/login.php');
     }
     //if the above code is false then code and html below will be executed
-    $booking = new Booking;
+    $booking= new Booking;
     //if add booking is submitted
     if(isset($_POST['save'])){
         //sanitize user inputs
@@ -25,14 +25,12 @@
         $booking->contact_number = $_POST['contact_number'];
         $booking->address = $_POST['address'];
         $booking->date = $_POST['date'];
-        if(isset($_POST['role'])){
-            $booking->admission_role = $_POST['role'];
-        }
+        
         if(isset($_POST['status'])){
             $booking->status = $_POST['status'];
         }
         if(validate_add_booking($_POST)){
-            if($fbooking->edit()){  
+            if($booking->edit()){  
                 //redirect user to booking page after saving
                 header('location: booking.php');
             }
@@ -51,7 +49,7 @@
     }
 
     require_once '../tools/variables.php';
-    $page_title = 'Lionico | Update Record';
+    $page_title = 'Forecast | Update booking';
     $booking = 'active';
 
     require_once '../includes/header.php';
@@ -66,54 +64,54 @@
                     <a class="back" href="booking.php"><i class='bx bx-caret-left'></i>Back</a>
                 </div>
                 <div class="add-form-container">
-                <form class="add-form" action="addbooking.php" method="post">
-                    <input type="text" hidden name="booking-id" value="<?php echo $data['id']; ?>">
-                    <label for="fn">First Name</label>
-                    <input type="text" id="fn" name="fn" required placeholder="Enter first name" value="<?php if(isset($_POST['fn'])) { echo $_POST['fn']; } ?>">
-                    <?php
-                        if(isset($_POST['save']) && !validate_first_name($_POST)){
-                    ?>
-                                <p class="error">First name is invalid. Trailing spaces will be ignored.</p>
-                    <?php
-                        }
-                    ?>
-                    <label for="ln">Last Name</label>
-                    <input type="text" id="ln" name="ln" required placeholder="Enter last name" value="<?php if(isset($_POST['ln'])) { echo $_POST['ln']; } ?>">
-                    <?php
-                        if(isset($_POST['save']) && !validate_last_name($_POST)){
-                    ?>
-                                <p class="error">Last name is invalid. Trailing spaces will be ignored.</p>
-                    <?php
-                        }
-                    ?>
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required placeholder="Enter email" value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } ?>">
-                    <?php
-                        if(isset($_POST['save']) && !validate_email($_POST)){
-                    ?>
-                                <p class="error">Email is invalid. Use only @lionico.ph</p>
-                    <?php
-                        }
-                    ?>
-                    <label for="contact_number">Contact Number</label>
-                    <input type="text" id="contact_number" name="contact_number" required placeholder="contact_number" value="<?php if(isset($_POST['contact_number'])) { echo $_POST['contact_number']; } ?>">
-                    <br>
+                    <form class="add-form" action="editbooking.php" method="post">
+                        <input type="text" hidden name="booking-id" value="<?php echo $data['id']; ?>">
+                        <label for="fn">First Name</label>
+                        <input type="text" id="fn" name="fn" required placeholder="Enter first name" value="<?php if(isset($_POST['fn'])) { echo $_POST['fn']; } else { echo $data['firstname']; }?>">
+                        <?php
+                            if(isset($_POST['save']) && !validate_first_name($_POST)){
+                        ?>
+                                    <p class="error">First name is invalid. Trailing spaces will be ignored.</p>
+                        <?php
+                            }
+                        ?>
+                        <label for="ln">Last Name</label>
+                        <input type="text" id="ln" name="ln" required placeholder="Enter last name" value="<?php if(isset($_POST['ln'])) { echo $_POST['ln']; } else { echo $data['lastname']; }?>">
+                        <?php
+                            if(isset($_POST['save']) && !validate_last_name($_POST)){
+                        ?>
+                                    <p class="error">Last name is invalid. Trailing spaces will be ignored.</p>
+                        <?php
+                            }
+                        ?>
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" required placeholder="Enter email" value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } else { echo $data['email']; }?>">
+                        <?php
+                            if(isset($_POST['save']) && !validate_email($_POST)){
+                        ?>
+                                    <p class="error">Email is invalid. Use only @lionico.ph</p>
+                        <?php
+                            }
+                        ?>
+                        <label for="contact_number">Contact Number</label>
+                        <input type="text" id="contact_number" name="contact_number" required placeholder="contact_number" value="<?php if(isset($_POST['contact_number'])) { echo $_POST['contact_number']; } ?>">
+                        <br>
 
-                    <label for="address">Address</label>
-                    <input type="text" id="address" name="address" required placeholder="address" value="<?php if(isset($_POST['address'])) { echo $_POST['address']; } ?>">
-                    <br>
+                        <label for="address">Address</label>
+                        <input type="text" id="address" name="address" required placeholder="address" value="<?php if(isset($_POST['address'])) { echo $_POST['address']; } ?>">
+                        <br>
 
-                    <label for="date">Date</label>
-                    <input type="date" id="date" name="date" required value="<?php if(isset($_POST['date'])) { echo $_POST['date']; } ?>">
-                    <br>
+                        <label for="date">Date</label>
+                        <input type="date" id="date" name="date" required value="<?php if(isset($_POST['date'])) { echo $_POST['date']; } ?>">
+                        <br>
 
-                    <label for="status">Is Status of Employee Active?</label><br>
-                    <label class="container" for="status">Yes
-                        <input type="checkbox" name="status" id="status" value="Active Employee" <?php if(isset($_POST['status'])) { if ($_POST['status'] == 'Active Employee') echo ' checked'; } ?>>
-                        <span class="checkbox"></span>
-                    </label>
-                    <input type="submit" class="button" value="Save Record" name="save" id="save">
-                </form>
+                        <label for="status">Is Status of Employee Active?</label><br>
+                        <label class="container" for="status">Yes
+                            <input type="checkbox" name="status" id="status" value="Active Employee" <?php if(isset($_POST['status'])) { if ($_POST['status'] == 'Active Employee') echo ' checked'; } elseif ($data['status'] == 'Active Employee') echo ' checked'; ?>>
+                            <span class="checkbox"></span>
+                        </label>
+                        <input type="submit" class="button" value="Save Record" name="save" id="save">
+                    </form>
                 </div>
             </div>
         </div>
