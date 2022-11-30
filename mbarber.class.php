ce9979b54bfadb46e899b45864a0c3e2f0420d5f
MarvinWaro <114147003+MarvinWaro@users.noreply.github.com>
2022-11-30 17:50:55 +0800

    no folder

---
+++ mbarber.class.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once 'database.php';
+
+class Mbarber{
+    //attributes
+
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $contact_number;
+    public $address;
+    public $chair_assigned;
+    public $status = 'Inactive';
+
+    protected $db;
+
+    function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    //Methods
+    function add(){
+        $sql = "INSERT INTO mbarber (firstname, lastname, email, contact_number, address, chair_assigned, status) VALUES
+        (:firstname, :lastname, :email, :contact_number, :address, :chair_assigned, :status);";
+
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':firstname', $this->firstname);
+        $query->bindParam(':lastname', $this->lastname);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':contact_number', $this->contact_number);
+        $query->bindParam(':address', $this->address);
+        $query->bindParam(':chair_assigned', $this->chair_assigned);
+        $query->bindParam(':status', $this->status);
+        
+        if($query->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }	
+    }
+
+    function edit(){
+        $sql = "UPDATE mbarber SET firstname=:firstname, lastname=:lastname, email=:email, contact_number=:contact_number, address=:address, chair_assigned=:chair_assigned, status=:status WHERE id = :id;";
+
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':firstname', $this->firstname);
+        $query->bindParam(':lastname', $this->lastname);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':contact_number', $this->contact_number);
+        $query->bindParam(':address', $this->address);
+        $query->bindParam(':chair_assigned', $this->chair_assigned);
+        $query->bindParam(':status', $this->status);
+        $query->bindParam(':id', $this->id);
+
+        if($query->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function fetch($record_id){
+        $sql = "SELECT * FROM mbarber WHERE id = :id;";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':id', $record_id);
+        if($query->execute()){
+            $data = $query->fetch();
+        }
+        return $data;
+    }
+
+    function delete($record_id){
+        $sql = "DELETE FROM mbarber WHERE id = :id;";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':id', $record_id);
+        if($query->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function show(){
+        $sql = "SELECT * FROM mbarber ORDER BY CONCAT('lastname',', ','firstname') ASC;";
+        $query=$this->db->connect()->prepare($sql);
+        if($query->execute()){
+            $data = $query->fetchAll();
+        }
+        return $data;
+    }
+
+}
+
+?>
---
+++ mbarber.php
@@ -0,0 +1,100 @@
+<?php
+
+    //resume session here to fetch session values
+    session_start();
+    /*
+        if user is not login then redirect to login page,
+        this is to prevent users from accessing pages that requires
+        authentication such as the dashboard
+    */
+    if (!isset($_SESSION['logged-in'])){
+        header('location: ../login/login.php');
+    }
+    //if the above code is false then html below will be displayed
+
+    require_once '../tools/variables.php';
+    $page_title = 'Lionico | Show Barbers';
+    $mbarber = 'active';
+
+    require_once '../includes/header.php';
+    require_once '../includes/sidebar.php';
+    require_once '../includes/topnav.php';
+?>
+    <div class="home-content">
+        <div class="table-container">
+            <div class="table-heading">
+                <h3 class="table-title">Barber's</h3>
+                <?php
+                    if($_SESSION['user_type'] == 'admin'){ 
+                ?>
+                    <a href="addmbarber.php" class="button">Add New Record</a>
+                <?php
+                    }
+                ?>
+            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Contact Number</th>
+                        <th>Address</th>
+                        <th>Chair Assigned</th>
+                        <th>Status</th>
+                        <?php
+                            if($_SESSION['user_type'] == 'admin'){ 
+                        ?>
+                            <th class="action">Action</th>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        require_once '../classes/mbarber.class.php';
+
+                        $mbarber = new Mbarber();
+                        //We will now fetch all the records in the array using loop
+                        //use as a counter, not required but suggested for the table
+                        $i = 1;
+                        //loop for each record found in the array
+                        foreach ($mbarber->show() as $value){ //start of loop
+                    ?>
+                        <tr>
+                            <!-- always use echo to output PHP values -->
+                            <td><?php echo $i ?></td>
+                            <td><?php echo $value['lastname'] . ', ' . $value['firstname'] ?></td>
+                            <td><?php echo $value['email'] ?></td>
+                            <td><?php echo $value['contact_number'] ?></td>
+                            <td><?php echo $value['address'] ?></td>
+                            <td><?php echo $value['chair_assigned'] ?></td>
+                            <td><?php echo $value['status'] ?></td>
+                            <?php
+                                if($_SESSION['user_type'] == 'admin'){ 
+                            ?>
+                                <td>
+                                    <div class="action">
+                                    <a class="action-edit" href="editmbarber.php?id=<?php echo $value['id'] ?>">Edit</a>
+                                    <a class="action-delete" href="deletembarber.php?id=<?php echo $value['id'] ?>">Delete</a>
+                                    </div>
+                                </td>
+                            <?php
+                                }
+                            ?>
+                        </tr>
+                    <?php
+                        $i++;
+                    //end of loop
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+<?php
+    require_once '../includes/bottomnav.php';
+    require_once '../includes/footer.php';
+?>
